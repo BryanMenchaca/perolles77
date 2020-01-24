726fa9c5debdef2da24ff8b6839908fc968b1dd9
BryanMenchaca <bryan.men11@gmail.com>
2020-01-24 16:54:50 -0600

    Perolles 77 - Proyecto

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+$("#item1").hover(function(){
+    $('.submenu1').css("display", "block");
+    }, function(){
+    $('.submenu1').css("display", "none");
+});
+
+$("#item2").hover(function(){
+    $('.submenu2').css("display", "block");
+    }, function(){
+    $('.submenu2').css("display", "none");
+});
